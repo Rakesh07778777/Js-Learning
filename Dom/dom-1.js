299c7636035566b6a7659e3f049c60c1b93f3e02
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-17 17:52:50 +0530

    Event-listener

--- Dom/dom-1.js
+++ Dom/dom-1.js
@@ -1,3 +1,4 @@
+/*
 const div = document.createElement('div')
 const button = document.createElement('button')
 const body = document.querySelector('body')
@@ -9,7 +10,29 @@ button.addEventListener('click', () => {
     console.log('hello')
 })
 
-// const para = document.createElement('p')
-// const text = document.createTextNode(
-//     " -Hey there how are you today")
-//     para.append(text)
+const fragement = document.createDocumentFragment()
+
+for(let i = 0; i < 10; i++){
+    const div = document.createElement('div')
+    div.textContent = `item ${i}`
+    fragement.appendChild(div)
+}
+
+document.body.appendChild(fragement)
+*/
+
+// Event addEventListener
+
+document.addEventListener('click', e => {
+  if ( e.target.matches('div')){
+    console.log('Hii')
+   }
+})
+
+const newDiv = document.createElement('div')
+const body = document.querySelector('body')
+newDiv.style.height = '300px'
+newDiv.style.width = '300px'
+newDiv.style.backgroundColor = '#111'
+
+body.append(newDiv)

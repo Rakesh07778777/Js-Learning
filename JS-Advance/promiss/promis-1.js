aa7198e13dd9448a1e8870ca450d78f1c509553a
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-27 22:36:37 +0530

    fetch

--- JS-Advance/promiss/promis-1.js
+++ JS-Advance/promiss/promis-1.js
@@ -42,8 +42,72 @@ thirdPromise.then((user) => {
 
 */
 
+/*
+
 const fourthPromise = new Promise((resolve , reject) => {
-    setInterval(() => {
-        
+    setTimeout(() => {
+        let error = true;
+
+        if(!error){
+            resolve({username: 'fgnrnasdg' , Id: 343})
+        }else{
+            reject('ERROR : something wrong with your codes check properly')
+        }
     })
 })
+
+fourthPromise
+.then((user) => {
+    console.log(user)
+    return user.username
+})
+.then((username) => {
+    console.log(username)
+})
+.catch((err) => {
+    console.log(err)
+})
+.finally()
+
+*/
+
+
+const promiseFive = new Promise((resolve , rejected) => {
+    setTimeout(() => {
+        let error = true;
+
+        if(!error){
+            resolve({username: 'fgnrnasdg' , Id: 343})
+        }else{
+            rejected('ERROR : Again something wrong with your codes check properly')
+        }
+    })
+})
+
+async function consumePromise(){
+    try {
+        const response = await promiseFive
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+consumePromise()
+
+
+async function getAllUsers() {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data)
+    }
+    catch (error) {
+        console.log(error)
+        console.log('Err :' , error)
+    }
+}
+
+getAllUsers()
+
+
+

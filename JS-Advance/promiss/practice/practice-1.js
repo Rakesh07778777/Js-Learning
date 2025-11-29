12005e607502cea3e125098d0eae1625d393b0c3
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-29 22:20:55 +0530

    practice 3

--- JS-Advance/promiss/practice/practice-1.js
+++ JS-Advance/promiss/practice/practice-1.js
@@ -1,4 +1,4 @@
-const task =  new Promise((resolve , reject) => {
+/*const task =  new Promise((resolve , reject) => {
     let isTask = true
     setTimeout(() => {
         if(!isTask){
@@ -15,4 +15,32 @@ task.then((message) => {
     console.log(error)
 })
 
+
+
+const task2 = new Promise((resolve, reject) => {
+    reject("Something went wrong")
+})
+
+task2
+  .then(msg => console.log(msg))
+  .catch(err => console.log(err))
+
 // console.log(task)
+
+*/
+
+const randomNUmber = new Promise((resolve , reject) => {
+    let number = 6
+    if(number > 5){
+        resolve('the number is greater than 5')
+    } else{
+        reject('the number is less than 5')
+    }
+})
+
+randomNUmber.then((nums) => {
+console.log(nums)
+})
+.catch((error) => {
+    console.log(error)
+})

203b65b709e9ef4c35c096b982123e365eaf76af
Rakesh07778777 <bossrakesh43@gmail.com>
2025-12-02 22:16:55 +0530

    clean

--- JS-Advance/random_joke_generator/random.js
+++ JS-Advance/random_joke_generator/random.js
@@ -1,15 +1,15 @@
 async function jokeGenerator() {
-    const firstDiv = document.querySelector('.punch-line')
-    const secondDiv = document.querySelector('.setup-joke')
-    const response = await fetch('https://official-joke-api.appspot.com/jokes/random')
 
     try {
+    const response = await fetch('https://official-joke-api.appspot.com/jokes/random')
         if(!response.ok){
             throw new Error("Data couldnot fetch");            
         }
             const data = await response.json()
             const punchLine = data.punchline
             const jokes = data.setup
+            const firstDiv = document.querySelector('.punch-line')
+            const secondDiv = document.querySelector('.setup-joke')
             firstDiv.style.padding = '1.2rem'
             secondDiv.style.padding = '1.2rem'
             firstDiv.innerText = punchLine

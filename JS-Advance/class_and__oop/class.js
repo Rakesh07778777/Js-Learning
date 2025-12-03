89a674066b3594b211f29e7d3525251dc23076e2
Rakesh07778777 <bossrakesh43@gmail.com>
2025-12-04 00:23:02 +0530

    classes

--- JS-Advance/class_and__oop/class.js
+++ JS-Advance/class_and__oop/class.js
@@ -1,3 +1,5 @@
+/*
+
 class User{
     constructor(username, email, password){
         this.username = username
@@ -8,6 +10,10 @@ class User{
     encryptPass(){
         return `${this.password}123`
     }
+
+    changerPass(){
+        return `${this.password.toUpperCase()}123`
+    }
 }
 
 const anotherUser = new User('Rakesh' , 'awrg@google.com' , 'abcd')
@@ -15,3 +21,29 @@ const anotherUser = new User('Rakesh' , 'awrg@google.com' , 'abcd')
 
 console.log(anotherUser)
 console.log(anotherUser.encryptPass())
+console.log(anotherUser.changerPass())
+
+
+*/
+
+function User(username, email, password){
+        this.username = username
+        this.email = email
+        this.password = password
+}
+
+Object.prototype.encryptPass = function (){
+        return `${this.password}123`  
+}
+
+Object.prototype.changerPass = function (){
+        return `${this.password.toUpperCase()}123`   
+}
+
+
+const anotherUser = new User('Rakesh' , 'awrg@google.com' , 'abcd')
+
+
+console.log(anotherUser)
+console.log(anotherUser.encryptPass())
+console.log(anotherUser.changerPass())
